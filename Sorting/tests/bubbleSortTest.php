0a12f15ac6ee2e283d6bd15916eccbd55a0b9815
Diego Ramos <name@example.com>
2023-08-31 18:45:42 +0200

    Add test of negative values

--- Sorting/tests/bubbleSortTest.php
+++ Sorting/tests/bubbleSortTest.php
@@ -11,28 +11,13 @@ class BubbleSortTest extends PHPUnit\Framework\TestCase {
         bubbleSort($testArray);
         $this->assertEquals($testArray, $expectedOutput);
     }
+    public function test_it_orders_negative_values()
+    {
+        $testArray = [-65,14,49,33,81,-17,13,-36,-83,64];
+        $expectedOutput = [-83,-65,-36,-17,13,14,33,49,64,81];
+
+        bubbleSort($testArray);
+        $this->assertEquals($testArray, $expectedOutput);
+    }
 }
-
-
-
-// Generate test array
-/*
-for ($i=0;$i<10000;$i++){
-    $testArray[$i]=random_int(0,10000);
-}
-
-// Test the algorithm
-$executionStart = microtime(true);
-memory_reset_peak_usage();
-bubbleSort($testArray);     
-$memoryUsage = memory_get_peak_usage()/1024/1024;    
-$executionTime = microtime(true) - $executionStart;
-
-// Show results
-for ($i=0;$i<sizeof($testArray);$i++){
-    echo "$testArray[$i] ";
-}
-echo "\n\nExecution time: ",$executionTime," seconds";
-echo "\nMemory usage: ",$memoryUsage," Megabytes";*/
-
 ?>
