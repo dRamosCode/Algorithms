876b285cff425828d8fd57b36807d81bb68cb3ac
Diego Ramos <name@example.com>
2023-08-31 18:50:54 +0200

    Add tests for different type of numbers

--- Sorting/tests/bubbleSortTest.php
+++ Sorting/tests/bubbleSortTest.php
@@ -19,5 +19,21 @@ class BubbleSortTest extends PHPUnit\Framework\TestCase {
         bubbleSort($testArray);
         $this->assertEquals($testArray, $expectedOutput);
     }
+    public function test_it_orders_float_values()
+    {
+        $testArray = [96.43,74.18,71.71,54.85,53.36,41.94,24.67,38.45,1.32,20.76];
+        $expectedOutput = [1.32, 20.76, 24.67, 38.45, 41.94, 53.36, 54.85, 71.71, 74.18, 96.43];
+
+        bubbleSort($testArray);
+        $this->assertEquals($testArray, $expectedOutput);
+    }
+    public function test_it_orders_a_mixture_of_numbers()
+    {
+        $testArray = [96.43,-74.18,71,54.85,53,41.9,-24.67,38,-1.32,-20];
+        $expectedOutput = [-74.18, -24.67, -20, -1.32, 38, 41.9, 53, 54.85, 71, 96.43];
+
+        bubbleSort($testArray);
+        $this->assertEquals($testArray, $expectedOutput);
+    }
 }
 ?>
